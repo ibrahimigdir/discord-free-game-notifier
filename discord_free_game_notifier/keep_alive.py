d31ca2f36da533a09af828f5867d182355d15fca
ibrahimigdir <59979086+ibrahimigdir@users.noreply.github.com>
2024-07-28 04:33:40 +0300

    Update keep_alive.py

--- discord_free_game_notifier/keep_alive.py
+++ discord_free_game_notifier/keep_alive.py
@@ -3,7 +3,7 @@ from threading import Thread
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('../pages/index.html')
+    return "Alive"
 def run():
   app.run(host='0.0.0.0',port=3000)
 def keep_alive():  
